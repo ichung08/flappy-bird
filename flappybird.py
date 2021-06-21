70a5293733d90f7e869b57678ddfe7dbbd2e2dbf
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-06-20 18:15:31 -0700

    adding game score

--- flappybird.py
+++ flappybird.py
@@ -44,17 +44,22 @@ def bird_animation():
 
 def score_display(game_state):
     if game_state == 'main_game':
-        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
         score_rect = score_surface.get_rect(center = (288, 100))
         screen.blit(score_surface, score_rect)
     if game_state == 'game_over':
-        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
         score_rect = score_surface.get_rect(center = (288, 100))
         screen.blit(score_surface, score_rect)
 
-        high_score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        high_score_surface = game_font.render(f'High Score: {int(high_score)}', True, (255, 255, 255))
         high_score_rect = score_surface.get_rect(center = (288, 850))
         screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
 
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
@@ -115,6 +120,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100, 512)
                 bird_movement = 0
+                score = 0
 
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
@@ -145,6 +151,7 @@ while True:
         score += 0.01
         score_display('main_game')
     else:
+        high_score = update_score(score, high_score)
         score_display('game_over')
 
     # Floor
